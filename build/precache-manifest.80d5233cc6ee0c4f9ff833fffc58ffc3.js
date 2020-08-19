c8ac994b06e01ff14f8da7f82d33d87183cf7da5
BibhuPatra <bibhuptr1@gmail.com>
2020-08-19 12:19:50 +0530

    Deploy my react app to git hub pages

---
+++ build/precache-manifest.80d5233cc6ee0c4f9ff833fffc58ffc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32726ee81d5eafa99a33cf8443613e29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e89ca984fd538cd20c13",
+    "url": "/static/css/main.ee7e7ac7.chunk.css"
+  },
+  {
+    "revision": "b04f4b1e00444b1e9801",
+    "url": "/static/js/2.b93bb5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b93bb5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e89ca984fd538cd20c13",
+    "url": "/static/js/main.155e9f07.chunk.js"
+  },
+  {
+    "revision": "fd1e764c12408bbbbfc1",
+    "url": "/static/js/runtime-main.f859b388.js"
+  }
+]);
